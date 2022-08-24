59f935dc4b69bd7347d6e790c6fd5b5a1a6363cc
Alexandra Lewis <alexandra.nadine.lewis@gmail.com>
2022-08-24 11:34:00 -0700

    added active classes for buttons, grid setup

---
+++ app.js
@@ -0,0 +1,13 @@
+let container = document.querySelector(".sketch");
+
+function createGrid(row, col) {
+  container.style.setProperty("--grid-row", row);
+  container.style.setProperty("--grid-col", col);
+
+  for (i = 0; i < row * col; i++) {
+    let cell = document.createElement("div");
+    container.appendChild(cell).className = "cells";
+  }
+}
+
+createGrid(16, 16)
